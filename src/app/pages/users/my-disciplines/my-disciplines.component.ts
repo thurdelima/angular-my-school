b5bc4e8c0a1a6c37a18a3e24c8d4bd620ae789b1
Arthur <thurdelima@gmail.com>
2024-10-08 01:34:44 -0300

    feat: new components

---
+++ src/app/pages/users/my-disciplines/my-disciplines.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-disciplines',
+  templateUrl: './my-disciplines.component.html',
+  styleUrl: './my-disciplines.component.scss'
+})
+export class MyDisciplinesComponent {
+
+}
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyConquestsComponent } from './my-conquests/my-conquests.component';
+import { MyDisciplinesComponent } from './my-disciplines/my-disciplines.component';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    MyProfileComponent,
+    MyConquestsComponent,
+    MyDisciplinesComponent
   ],
   imports: [
     CommonModule,
