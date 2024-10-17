2ef593e5f6cbb1ab8b767763821fab87d2c45485
Arthur <thurdelima@gmail.com>
2024-10-17 01:33:33 -0300

    feat: adjust some tabs

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styles: ``
+  styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
 
