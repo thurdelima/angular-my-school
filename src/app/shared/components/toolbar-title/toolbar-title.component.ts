ac89e93128c2a09ac49d507cad9adbe357f9d0c0
Arthur <thurdelima@gmail.com>
2024-10-29 03:37:26 -0300

    feat: instaling fontawesome

--- src/app/shared/components/toolbar-title/toolbar-title.component.ts
+++ src/app/shared/components/toolbar-title/toolbar-title.component.ts
@@ -3,11 +3,23 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-toolbar-title',
   templateUrl: './toolbar-title.component.html',
-  styleUrl: './toolbar-title.component.scss'
+  styleUrls: ['./toolbar-title.component.scss']
 })
 export class ToolbarTitleComponent {
 
+  public iconFa: string = '';
+  public iconMat: string= '';
+
   @Input() title: string = "";
-  @Input() icon: string = "";
+
+
+  @Input()
+  set icon(value: string) {
+    value.includes('fa-') ? this.iconFa = `icon-space-mat ${value}` : this.iconMat = value;
+
+  }
+
+
+
 
 }
