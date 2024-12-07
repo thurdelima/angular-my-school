abc69666908155630798847e6cc1a38a8cb51429
Arthur <thurdelima@gmail.com>
2024-12-07 16:32:59 -0300

    feat: create other operations of service

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Course } from '@app/shared/models/course';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,25 @@ export class CoursesService {
   baseUrl =  `${environment.apiUrl}courses`;
   private http = inject(HttpClient);
 
-  public getCoursesById(id: number): Observable<Course[]> {
-    return this.http.get<Course[]>{`${this.baseUrl}/${id}`};
+  public get(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseUrl}`);
   }
 
-  constructor() { }
+  public getById(id: number): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseUrl}/${id}`);
+  }
+
+  public post(course: Course): Observable<Course[]> {
+    return this.http.post<Course[]>(`${this.baseUrl}`, course);
+  }
+
+  public put(id: number, course: Course): Observable<Course[]> {
+    return this.http.put<Course[]>(`${this.baseUrl}/${id}`, course);
+  }
+
+  public delete(id: number): Observable<Course[]> {
+    return this.http.delete<Course[]>(`${this.baseUrl}/${id}`);
+  }
+
+
 }
