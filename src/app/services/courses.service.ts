c4420d8e39a0215b12369159f522de88c873e264
Arthur <thurdelima@gmail.com>
2024-12-07 15:36:29 -0300

    feat: adjust course service and his types

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  baseUrl =  `${environment.apiUrl}courses`;
+  private http = inject(HttpClient);
+
+  public getCoursesById(id: number): Observable<Course[]> {
+    return this.http.get<Course[]>{`${this.baseUrl}/${id}`};
+  }
 
   constructor() { }
 }
